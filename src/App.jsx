6d72622d1a5804725cc0bdd5e675ade1c8ea0ed8
Ariel Campos <ariel.campos94@yahoo.com>
2024-04-26 10:58:36 -0300

    fixed background bug in sign up/ sign in page

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/theme/default'
 import { GlobalStyled } from './styles/global'
 
-import { Home } from './pages/Home'
+// import { Home } from './pages/Home'
 // import { SignUp } from './pages/SignUp'
+// import { SignIn } from './pages/SignIn'
 
 export function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={defaultTheme}>
         <GlobalStyled />
+        {/* <SignIn /> */}
         {/* <SignUp /> */}
-        <Home />
+        {/* <Home /> */}
       </ThemeProvider>
     </BrowserRouter>
   )
--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -6,8 +6,17 @@ export const SignUpContainer = styled.div`
   max-width: 26.75rem;
   padding: 9.875rem 2.9375rem 0 4.0625rem;
   margin: 0 auto;
-  height: 100vh;
   width: 100%;
+
+  > a {
+    img {
+      height: 43px;
+      width: 43px;
+    }
+    span {
+      font-size: 2.35rem;
+    }
+  }
 `
 
 export const CompleteSignUp = styled.form`
